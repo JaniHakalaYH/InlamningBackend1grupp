06663fda4f367e230e63e2f8a28e2d1cbe23689b
JaniHakalaYH <jani.hakala@yh.nackademin.se>
2023-04-24 11:01:50 +0200

    first commit

---
+++ src/main/java/com/example/inlamningbackend1/InlamningBackend1Application.java
@@ -0,0 +1,15 @@
+package com.example.inlamningbackend1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InlamningBackend1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InlamningBackend1Application.class, args);
+    }
+
+
+    //hej mina vännor
+}
---
+++ src/test/java/com/example/inlamningbackend1/InlamningBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inlamningbackend1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InlamningBackend1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
